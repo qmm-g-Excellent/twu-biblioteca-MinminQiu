167417e720a163453ee157087d3b3139b8011e59
Minmin Qiu <mmqiu@thoughtworks.com>
2018-03-05 11:27:19 +0800

    make MainMenuTest passed

--- src/com/twu/biblioteca/businessCase/MainMenu.java
+++ src/com/twu/biblioteca/businessCase/MainMenu.java
@@ -2,13 +2,15 @@ package com.twu.biblioteca.businessCase;
 
 public class MainMenu {
 
-    public void homeMessage() {
+    public void mainMenu() {
         System.out.printf("******************************************\n\n");
 
         System.out.printf("%-20s", "Welcome to Biblioteca Library Management System!\n\n");
+        System.out.printf("0.回到主菜单\n");
         System.out.printf("1.查看书籍列表\n");
         System.out.printf("2.借书\n");
         System.out.printf("3.还书\n");
+        System.out.printf("4.退出\n");
 
         System.out.printf("\n请输入你的操作代号：");
     }
---
+++ test/com/twu/biblioteca/businessCase/MainMenuTest.java
@@ -0,0 +1,33 @@
+package com.twu.biblioteca.businessCase;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainMenuTest {
+
+    @Test
+    public void should_print_main_menu_content_contains_checkout_book() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        new MainMenu().mainMenu();
+        assertThat(outContent.toString().contains("借书"), is(true));
+    }
+
+    @Test
+    public void should_print_main_menu_content_contains_return_book() {
+
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        new MainMenu().mainMenu();
+        assertThat(outContent.toString().contains("还书"), is(true));
+    }
+
+}
