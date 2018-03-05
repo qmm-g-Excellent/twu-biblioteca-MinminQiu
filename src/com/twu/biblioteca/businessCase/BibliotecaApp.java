09ec082919a82206b2bc36d4d78a221b7f7b4134
Minmin Qiu <mmqiu@thoughtworks.com>
2018-03-05 12:05:29 +0800

    refactor code

--- src/com/twu/biblioteca/businessCase/BibliotecaApp.java
+++ src/com/twu/biblioteca/businessCase/BibliotecaApp.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class BibliotecaApp {
 
+    private final static Integer mainMenu = 0;
     private final static Integer showBookListNo = 1;
     private final static Integer checkoutBookNo = 2;
     private final static Integer returnBookNo = 3;
+    private final static Integer quit = 4;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,22 +18,29 @@ public class BibliotecaApp {
         int operationNum = scanner.nextInt();
 
         do {
-            if (operationNum == 0) {
-                mainMenu.mainMenu();
-            } else if (operationNum == showBookListNo) {
-                ShowBooks showBooks = new ShowBooks();
-                showBooks.showBooks();
-            } else if (operationNum == checkoutBookNo) {
-                new CheckoutBook().CheckoutOneBook(new Scanner(System.in));
-            } else if (operationNum == returnBookNo) {
-                new ReturnBook().returnBook(new Scanner(System.in));
-            } else {
-                System.out.println("Select a valid option!");
+
+            switch (operationNum) {
+                case 0:
+                    mainMenu.mainMenu();
+                    break;
+                case 1:
+                    new ShowBooks().showBooks();
+                    break;
+                case 2:
+                    new CheckoutBook().CheckoutOneBook(new Scanner(System.in));
+                    break;
+                case 3:
+                    new ReturnBook().returnBook(new Scanner(System.in));
+                    break;
+                default:
+                    System.out.println("Select a valid option!");
+                    System.out.print("\n请输入你的操作代号：");
+                    break;
+
             }
 
             operationNum = scanner.nextInt();
         } while (operationNum != 4);
-
     }
 
 }
