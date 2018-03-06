a52b3d79f95fe7036e5a0051689ee8e49dc2ce35
Minmin Qiu <mmqiu@thoughtworks.com>
2018-03-06 15:19:18 +0800

    release 2: add limit for only login user can checkout book

--- src/com/twu/biblioteca/book/businessCase/CheckoutBook.java
+++ src/com/twu/biblioteca/book/businessCase/CheckoutBook.java
@@ -15,19 +15,23 @@ public class CheckoutBook {
         System.out.println("\n\n******************************************\n");
         System.out.print("请输入您要借出的书籍编号：");
         int bookNo = Integer.parseInt(scanner.nextLine());
-
-        for (Book book : new ArrayList<Book>(books)) {
-            if (book.getBookNo() == bookNo) {
-                book.setIsAvailable(false);
-                Set<Integer> keySet = BibliotecaApp.loginUser.asMap().keySet();
-                book.setUserId(getCurrentUser(keySet));
-                System.out.println("\nThank you! Enjoy the book！");
-                break;
-            }
-            if (books.indexOf(book) == (books.size() - 1)) {
-                System.out.println("\nThat book is not available");
+        Set<Integer> keySet = BibliotecaApp.loginUser.asMap().keySet();
+        if (keySet.size() <= 0) {
+            System.out.println("您当前未登陆，无法进行借书，请先登陆！");
+        } else {
+            for (Book book : new ArrayList<Book>(books)) {
+                if (book.getBookNo() == bookNo) {
+                    book.setIsAvailable(false);
+                    book.setUserId(getCurrentUser(keySet));
+                    System.out.println("\nThank you! Enjoy the book！");
+                    break;
+                }
+                if (books.indexOf(book) == (books.size() - 1)) {
+                    System.out.println("\nThat book is not available");
+                }
             }
         }
+
         System.out.print("\n请输入你的操作代号：");
     }
 
