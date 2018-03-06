7cf11fcfb9a89eec313891adcfc55d6a59df1d87
Minmin Qiu <mmqiu@thoughtworks.com>
2018-03-06 12:20:40 +0800

    release 2: add show movies

--- src/com/twu/biblioteca/main/BibliotecaApp.java
+++ src/com/twu/biblioteca/main/BibliotecaApp.java
@@ -1,13 +1,17 @@
-package com.twu.biblioteca;
+package com.twu.biblioteca.main;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.twu.biblioteca.book.businessCase.CheckoutBook;
-import com.twu.biblioteca.book.businessCase.MainMenu;
 import com.twu.biblioteca.book.businessCase.ReturnBook;
 import com.twu.biblioteca.book.businessCase.ShowBooks;
 import com.twu.biblioteca.movie.businessCase.CheckoutMovie;
 import com.twu.biblioteca.movie.businessCase.ShowMovie;
+import com.twu.biblioteca.user.businessCase.Login;
+import com.twu.biblioteca.user.entity.User;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class BibliotecaApp {
 
@@ -17,6 +21,11 @@ public class BibliotecaApp {
     private final static Integer returnBookNo = 3;
     private final static Integer quit = 4;
 
+    public static Cache<Integer, User> loginUser = CacheBuilder
+            .newBuilder()
+            .expireAfterAccess(1, TimeUnit.HOURS)
+            .build();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -25,10 +34,12 @@ public class BibliotecaApp {
         int operationNum = scanner.nextInt();
 
         do {
-
             switch (operationNum) {
                 case 0:
                     mainMenu.mainMenu();
+                    break;
+                case 1:
+                    new Login().login();
                     break;
                 case 2:
                     new ShowBooks().showBooks();
@@ -37,6 +48,9 @@ public class BibliotecaApp {
                     new CheckoutBook().CheckoutOneBook(new Scanner(System.in));
                     break;
                 case 4:
+                    new ReturnBook().returnBook(new Scanner(System.in));
+                    break;
+                case 5:
                     new ReturnBook().returnBook(new Scanner(System.in));
                     break;
                 case 6:
--- src/com/twu/biblioteca/movie/businessCase/ShowMovie.java
+++ src/com/twu/biblioteca/movie/businessCase/ShowMovie.java
@@ -1,6 +1,6 @@
 package com.twu.biblioteca.movie.businessCase;
 
-import com.twu.biblioteca.common.InitMovies;
+import com.twu.biblioteca.common.InitMovie;
 import com.twu.biblioteca.movie.entity.Movie;
 
 import java.util.List;
@@ -8,7 +8,7 @@ import java.util.List;
 public class ShowMovie {
 
     public void showMovies() {
-        List<Movie> movies = InitMovies.movies;
+        List<Movie> movies = InitMovie.movies;
 
         System.out.println("******************************************\n\n");
         System.out.format("%15s%15s%28s%15s%15s\n\n", "电影编号", '\t' + "电影名称", '\t' + "时间",  "导演" ,  "评分");
